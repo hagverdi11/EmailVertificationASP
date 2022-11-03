8658afde308bc11d9ab63c4b63ab2c60f3396016
hagverdi11 <hagverdiam@code.edu.az>
2022-11-04 02:31:02 +0400

    Added task

---
+++ EmbryoFrontToBack/EmbryoFrontToBack/Controllers/AccountController.cs
@@ -0,0 +1,166 @@
+﻿using EmbryoFrontToBack.Models;
+using EmbryoFrontToBack.ViewModels.AccountViewModels;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MimeKit.Text;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(registerVM);
+            }
+
+            AppUser user = new AppUser
+            {
+                FullName = registerVM.Fullname,
+                UserName = registerVM.Username,
+                Email = registerVM.Email,
+            };
+
+            IdentityResult result = await _userManager.CreateAsync(user, registerVM.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+                return View(registerVM);
+            }
+
+
+            string token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+            string link = Url.Action(nameof(ConfirmEmail), "Account", new { userId = user.Id, token }, 
+                Request.Scheme, Request.Host.ToString());
+
+
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse("haqverdiam@code.edu.az"));
+            email.To.Add(MailboxAddress.Parse(user.Email));
+            email.Subject = "Verify Email";
+            string body =string.Empty;
+
+            using (StreamReader reader = new StreamReader("wwwroot/templates/verify.html"))
+            {
+                body = reader.ReadToEnd();
+            }
+
+            body = body.Replace("{{link}}", link);
+            body = body.Replace("{{fullname}}", user.FullName);
+
+            email.Body = new TextPart(TextFormat.Html) { Text = body };
+
+            // send email
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate("hagverdiam@code.edu.az", "Yanaq12345");
+            smtp.Send(email);
+            smtp.Disconnect(true);
+
+            //await _signInManager.SignInAsync(user, false);
+
+
+            return RedirectToAction(nameof(VerifyEmail));
+        }
+
+        public async Task<IActionResult> ConfirmEmail(string userId, string token)
+        {
+            if (userId == null || token == null) return BadRequest();
+            AppUser user = await _userManager.FindByIdAsync(userId);
+            if (user == null) return NotFound();
+
+            await _userManager.ConfirmEmailAsync(user, token);
+            await _signInManager.SignInAsync(user, false);
+            return RedirectToAction("Index","Home");
+        }
+
+        public IActionResult VerifyEmail()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (!ModelState.IsValid) return View(loginVM);
+
+            AppUser user = await _userManager.FindByEmailAsync(loginVM.UsernameOrEmail);
+
+            if (user is null)
+            {
+                user = await _userManager.FindByNameAsync(loginVM.UsernameOrEmail);
+            }
+
+            if (user is null)
+            {
+                ModelState.AddModelError("", "Email or password wrong");
+                return View(loginVM);
+            }
+
+
+
+            var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Email or password wrong");
+                return View(loginVM);
+            }
+
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
+
+
+    }
+}
